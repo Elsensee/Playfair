accc22a9e29462dd3d2f4dee0ab5d99bc1e94842
Oliver Schramm <oliver.schramm97@gmail.com>
2013-06-04 22:23:29 +0200

    Refactoring nearly all of it and make it faster
    At least I hope I maked it faster.. But after I read a few articles it
    has to be.

--- Playfair/Playfair.cs
+++ Playfair/Playfair.cs
@@ -24,15 +24,7 @@ namespace Playfair
 		const int Ø = 216;
 		const int Ü = 220;
 		const int ß = 223;
-		char[,] playfairSquare = new char[5, 5];
-
-		public char[,] PlayfairSquare
-		{
-			get
-			{
-				return this.playfairSquare;
-			}
-		}
+		char[] playfairLine;
 
 		/// <summary>
 		/// Creates a new instance of Playfair class.
@@ -40,7 +32,7 @@ namespace Playfair
 		/// <param name="keyword">The secret keyword.</param>
 		public Playfair(string keyword)
 		{
-			this.playfairSquare = CreatePlayfairSquare(keyword);
+			this.playfairLine = CreatePlayfairLine(keyword);
 		}
 
 		/// <summary>
@@ -50,143 +42,7 @@ namespace Playfair
 		/// <returns>Returns the encrypted text.</returns>
 		public string Cipher(string plaintext)
 		{
-			plaintext = plaintext.ToUpper();
-			int corrections = 0;
-			if (plaintext.EndsWith("\0"))
-			{
-				corrections = -1;
-			}
-			List<char> chars = new List<char>(plaintext.Length + corrections + ((plaintext.Length + corrections) & 1));
-			#region plaintext preperations
-			bool inUmlaut = false;
-			// We don't know how long the text could be, but 2 BILLION characters.. wow.. that MUST be enough
-			// If not, somebody wants to cipher the bible.. Don't even think about it!
-			for (int i = 0; i < plaintext.Length; i++)
-			{
-				char c = plaintext[i];
-
-				// Converting umlauts into two chars
-				switch ((int)c)
-				{
-					// Ä or AE -> AE
-					case 196:
-					case 198:
-						if (!inUmlaut)
-						{
-							c = 'A';
-							i--;
-						}
-						else
-						{
-							c = 'E';
-						}
-						inUmlaut = !inUmlaut;
-						break;
-					// Ö -> OE
-					case 214:
-						if (!inUmlaut)
-						{
-							c = 'O';
-							i--;
-						}
-						else
-						{
-							c = 'E';
-						}
-						inUmlaut = !inUmlaut;
-						break;
-					// Ü -> UE
-					case 220:
-						if (!inUmlaut)
-						{
-							c = 'U';
-							i--;
-						}
-						else
-						{
-							c = 'E';
-						}
-						inUmlaut = !inUmlaut;
-						break;
-					// ß -> SS
-					case 223:
-						if (!inUmlaut)
-							i--;
-						c = 'S';
-						inUmlaut = !inUmlaut;
-						break;
-				}
-				if ((int)c < A || (int)c > Z)
-				{
-					corrections--;
-					continue;
-				}
-
-				// Tricky.. what if the char before is the same and its the last char of this pair?
-				if ((chars.Count & 1) == 1 && chars[chars.Count - 1] == c && chars[chars.Count - 1] != 'X')
-				{
-					chars.Add('X');
-					i--;
-					inUmlaut = !inUmlaut;
-					continue;
-				}
-
-				if (inUmlaut)
-					corrections++;
-
-				chars.Add(c);
-			}
-
-			if (((plaintext.Length + corrections) & 1) == 1)
-				chars.Add('X');
-			#endregion
-			StringBuilder result = new StringBuilder(chars.Count);
-			#region Encryption
-			int[] bufferPos = new int[2];
-			int[] pos = new int[2];
-			bool even = false;
-			for (int i = 0; i < chars.Count; i++)
-			{
-				if (!even)
-				{
-					even = true;
-					bufferPos = SearchInSquare(chars[i], this.playfairSquare);
-					continue;
-				}
-				even = false;
-				pos = SearchInSquare(chars[i], this.playfairSquare);
-				// Last letters are identical... (possible for XX)
-				if (bufferPos[0] == pos[0] && bufferPos[1] == pos[1])
-				{
-					bufferPos[0] -= ((++bufferPos[0] * 7) >> 5) * 5;
-					bufferPos[1] -= ((++bufferPos[1] * 7) >> 5) * 5;
-					pos[0] -= ((++pos[0] * 7) >> 5) * 5;
-					pos[1] -= ((++pos[1] * 7) >> 5) * 5;
-				}
-				// Same row
-				else if (bufferPos[0] == pos[0])
-				{
-					bufferPos[1] -= ((++bufferPos[1] * 7) >> 5) * 5;
-					pos[1] -= ((++pos[1] * 7) >> 5) * 5;
-				}
-				// Same column
-				else if (bufferPos[1] == pos[1])
-				{
-					bufferPos[0] -= ((++bufferPos[0] * 7) >> 5) * 5;
-					pos[0] -= ((++pos[0] * 7) >> 5) * 5;
-				}
-				// Anything else
-				else
-				{
-					int buffer = bufferPos[1]; // Again...
-					bufferPos[1] = pos[1];
-					pos[1] = buffer;
-				}
-				result.Append(this.playfairSquare[bufferPos[0], bufferPos[1]]);
-				result.Append(this.playfairSquare[pos[0], pos[1]]);
-			}
-			#endregion
-			return result.ToString();
+			return Cipher(plaintext, this.playfairLine);
 		}
 
 		/// <summary>
@@ -194,61 +50,136 @@ namespace Playfair
 		/// </summary>
 		/// <param name="encryptedText">Text which should be decrypted.</param>
 		/// <returns>Returns the decrypted text.</returns>
-		public string Decipher(string encryptedText)
+		public string Decipher(string plaintext)
 		{
-			encryptedText = encryptedText.ToUpper();
-			StringBuilder result = new StringBuilder(encryptedText.Length);
+			return Decipher(plaintext, this.playfairLine);
+		}
 
+		/// <summary>
+		/// Ciphers a text.
+		/// </summary>
+		/// <param name="plaintext">Text which should be encrypted.</param>
+		/// <param name="playfairLine">Playfair Line which is created off the keyword.</param>
+		/// <returns>Returns the encrypted text.</returns>
+		public static string Cipher(string plaintext, char[] playfairLine)
+		{
+			char[] text = CleanString(plaintext);
+			StringBuilder result = new StringBuilder(text.Length);
 			int[] bufferPos = new int[2];
 			int[] pos = new int[2];
 			bool even = false;
-			for (int i = 0; i < encryptedText.Length; i++)
+			for (int i = 0; i < text.Length; i++)
 			{
 				if (!even)
 				{
 					even = true;
-					bufferPos = SearchInSquare(encryptedText[i], this.playfairSquare);
+					bufferPos = SearchInLine(text[i], playfairLine);
 					continue;
 				}
 				even = false;
-				pos = SearchInSquare(encryptedText[i], this.playfairSquare);
-				// Last letters are identical... (possible for XX)
-				if (bufferPos[0] == pos[0] && bufferPos[1] == pos[1])
+				pos = SearchInLine(text[i], playfairLine);
+				// We can put two if-clauses into one :O
+				if (bufferPos[0] == pos[0])
 				{
-					bufferPos[0] += 9;
-					bufferPos[0] -= ((bufferPos[0] * 7) >> 5) * 5;
-					bufferPos[1] += 9;
-					bufferPos[1] -= ((bufferPos[1] * 7) >> 5) * 5;
-					pos[0] += 9;
-					pos[0] -= ((pos[0] * 7) >> 5) * 5;
-					pos[1] += 9;
-					pos[1] -= ((pos[1] * 7) >> 5) * 5;
+					if (bufferPos[1] == pos[1])
+					{
+						// Last letters are identical... (possible for XX)
+						bufferPos[0] -= ((++bufferPos[0] * 7) >> 5) * 5;
+						bufferPos[1] -= ((++bufferPos[1] * 7) >> 5) * 5;
+						pos[0] -= ((++pos[0] * 7) >> 5) * 5;
+						pos[1] -= ((++pos[1] * 7) >> 5) * 5;
+						result.Append(playfairLine[(bufferPos[0] * 5) + bufferPos[1]]);
+						result.Append(playfairLine[(pos[0] * 5) + pos[1]]);
+						continue;
+					}
+					// Same row
+					bufferPos[1] -= ((++bufferPos[1] * 7) >> 5) * 5;
+					pos[1] -= ((++pos[1] * 7) >> 5) * 5;
 				}
-				// Same row
-				else if (bufferPos[0] == pos[0])
-				{
-					bufferPos[1] += 9;
-					bufferPos[1] -= ((bufferPos[1] * 7) >> 5) * 5;
-					pos[1] += 9;
-					pos[1] -= ((pos[1] * 7) >> 5) * 5;
-				}
-				// Same column
 				else if (bufferPos[1] == pos[1])
 				{
-					bufferPos[0] += 9;
-					bufferPos[0] -= ((bufferPos[0] * 7) >> 5) * 5;
-					pos[0] += 9;
-					pos[0] -= ((pos[0] * 7) >> 5) * 5;
+					// Same column
+					bufferPos[0] -= ((++bufferPos[0] * 7) >> 5) * 5;
+					pos[0] -= ((++pos[0] * 7) >> 5) * 5;
 				}
-				// Anything else
 				else
 				{
-					int buffer = bufferPos[1]; // Again...
+					// Anything else
+					int buffer = bufferPos[1];
 					bufferPos[1] = pos[1];
 					pos[1] = buffer;
 				}
-				result.Append(this.playfairSquare[bufferPos[0], bufferPos[1]]);
-				result.Append(this.playfairSquare[pos[0], pos[1]]);
+				result.Append(playfairLine[(bufferPos[0] * 5) + bufferPos[1]]);
+				result.Append(playfairLine[(pos[0] * 5) + pos[1]]);
+			}
+			return result.ToString();
+		}
+
+		/// <summary>
+		/// Deciphers a text.
+		/// </summary>
+		/// <param name="encryptedText">Text which should be decrypted.</param>
+		/// <param name="playfairLine">Playfair Line which is created off the keyword.</param>
+		/// <returns>Returns the decrypted text.</returns>
+		public static string Decipher(string encryptedText, char[] playfairLine)
+		{
+			char[] text = CleanString(encryptedText);
+			StringBuilder result = new StringBuilder(text.Length);
+
+			int[] bufferPos = new int[2];
+			int[] pos = new int[2];
+			bool even = false;
+			for (int i = 0; i < text.Length; i++)
+			{
+				if (!even)
+				{
+					even = true;
+					bufferPos = SearchInLine(text[i], playfairLine);
+					continue;
+				}
+				even = false;
+				pos = SearchInLine(encryptedText[i], playfairLine);
+				// We can put two if-clauses into one :O
+				if (bufferPos[0] == pos[0])
+				{
+					if (bufferPos[1] == pos[1])
+					{
+						// Last letters are identical... (possible for XX)
+						bufferPos[0] += 9;
+						bufferPos[0] -= ((bufferPos[0] * 7) >> 5) * 5;
+						bufferPos[1] += 9;
+						bufferPos[1] -= ((bufferPos[1] * 7) >> 5) * 5;
+						pos[0] += 9;
+						pos[0] -= ((pos[0] * 7) >> 5) * 5;
+						pos[1] += 9;
+						pos[1] -= ((pos[1] * 7) >> 5) * 5;
+						result.Append(playfairLine[(bufferPos[0] * 5) + bufferPos[1]]);
+						result.Append(playfairLine[(pos[0] * 5) + pos[1]]);
+						continue;
+					}
+					// Same row
+					bufferPos[1] += 9;
+					bufferPos[1] -= ((bufferPos[1] * 7) >> 5) * 5;
+					pos[1] += 9;
+					pos[1] -= ((pos[1] * 7) >> 5) * 5;
+				}
+				else if (bufferPos[1] == pos[1])
+				{
+					// Same column
+					bufferPos[0] += 9;
+					bufferPos[0] -= ((bufferPos[0] * 7) >> 5) * 5;
+					pos[0] += 9;
+					pos[0] -= ((pos[0] * 7) >> 5) * 5;
+				}
+				else
+				{
+					// Anything else
+					int buffer = bufferPos[1];
+					bufferPos[1] = pos[1];
+					pos[1] = buffer;
+				}
+				result.Append(playfairLine[(bufferPos[0] * 5) + bufferPos[1]]);
+				result.Append(playfairLine[(pos[0] * 5) + pos[1]]);
 			}
 			return result.ToString();
 		}
@@ -261,7 +192,7 @@ namespace Playfair
 		/// <returns>Returns the encrypted text.</returns>
 		public static string Cipher(string plaintext, string keyword)
 		{
-			return new Playfair(keyword).Cipher(plaintext);
+			return Cipher(plaintext, CreatePlayfairLine(keyword));
 		}
 
 		/// <summary>
@@ -272,72 +203,27 @@ namespace Playfair
 		/// <returns>Returns the decrpyted text.</returns>
 		public static string Decipher(string encryptedText, string keyword)
 		{
-			return new Playfair(keyword).Decipher(encryptedText);
+			return Decipher(encryptedText, CreatePlayfairLine(keyword));
 		}
 
 		/// <summary>
-		/// Searches in a Square
-		/// </summary>
-		/// <param name="c">The char which I'm looking for...</param>
-		/// <param name="square">The square in which I'm looking for the char c...</param>
-		/// <returns>Returns the position in a two elements big byte-array.</returns>
-		private int[] SearchInSquare(char c, char[,] square)
-		{
-			for (int i = 0; i < square.GetLength(0); i++)
-			{
-				for (int j = 0; j < square.GetLength(1); j++)
-				{
-					if (square[i, j] == c)
-						return new int[] { i, j };
-				}
-			}
-			return null;
-		}
-
-		/// <summary>
-		/// Creates a Playfair square with the provided keyword.
+		/// Creates a Playfair line with the provided keyword.
 		/// </summary>
 		/// <param name="keyword">The secret keyword.</param>
-		/// <returns>Returns a char array containing the Playfair square</returns>
-		private char[,] CreatePlayfairSquare(string keyword)
+		/// <returns>Returns a char array containing the Playfair line</returns>
+		public static char[] CreatePlayfairLine(string keyword)
 		{
-			char[,] result = new char[5, 5];
-			char[] playfairLine = ToNoRepeatCharArray(CleanString(keyword) + ALPHABET);
-
-			if (playfairLine.Length > 25)
-			{
-				throw new ApplicationException("Internal Error! Array too big! (more than 25 elements)");
-			}
-			// Even if we don't need that much... It costs much time to convert it into integer... 
-			// So we have to draw a line... Less time or less memory?
-			for (int i = 0; i < 5; i++)
-			{
-				for (int j = 0; j < 5; j++)
-				{
-					result[i, j] = playfairLine[(i * 5) + j];
-				}
-			}
-			return result;
-		}
-
-		/// <summary>
-		/// Creates a char array from the string text in which every char shows up only one time and the J is replaced with I.
-		/// </summary>
-		/// <param name="text">The text which should be converted into a char array without repeations.</param>
-		/// <returns>Returns a char array without any repeations.</returns>
-		private char[] ToNoRepeatCharArray(string text)
-		{
-			// Let's reserve some space because changing size of arrays is slow and needs more space
-			char[] input = text.ToCharArray();
+			char[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+			char[] input = CleanString(keyword);
 			char[] result = new char[25];
 			bool[] alreadyGiven = new bool[25];
 
 			int pointer = 0;
 			int value = 0;
-			for (int i = 0; i < text.Length; i++)
+			for (int i = 0; i < input.Length; i++)
 			{
 				value = (int)input[i];
-				// Replace J with I (classical...)
+				// Replace J with I (classical)
 				if (value >= J)
 				{
 					value--;
@@ -348,12 +234,13 @@ namespace Playfair
 					result[pointer++] = input[i];
 				}
 			}
-			// Hopefully this condition will never be true...
-			if (pointer < 25)
+			for (int i = 0; i < input.Length; i++)
 			{
-				Array.Resize<char>(ref result, pointer);
+				if (!alreadyGiven[i])
+				{
+					result[pointer++] = alphabet[i];
+				}
 			}
-
 			return result;
 		}
 
@@ -362,87 +249,72 @@ namespace Playfair
 		/// </summary>
 		/// <param name="text">The text which schould be cleaned.</param>
 		/// <returns>Cleaned string.</returns>
-		private string CleanString(string text)
+		private static char[] CleanString(string text)
 		{
 			char[] input = text.ToUpper().ToCharArray();
-			StringBuilder result = new StringBuilder(text.Length);
-			bool inUmlaut = false;
-			int c;
+			List<char> result = new List<char>(input.Length);
 			for (int i = 0; i < text.Length; i++)
 			{
-				c = (int)input[i];
-
 				// Convert umlauts into two characters
-				switch (c)
+				switch (input[i])
 				{
 					// Ä or Æ -> AE
-					case Ä:
-					case Æ:
-						if (!inUmlaut)
-						{
-							c = A;
-							i--;
-						}
-						else
-						{
-							c = E;
-						}
-						inUmlaut = !inUmlaut;
-						break;
+					case 'Ä':
+					case 'Æ':
+						result.Add('A');
+						result.Add('E');
+						continue;
 					// Ö or Ø -> OE
-					case Ö:
-					case Ø:
-						if (!inUmlaut)
-						{
-							c = O;
-							i--;
-						}
-						else
-						{
-							c = E;
-						}
-						inUmlaut = !inUmlaut;
-						break;
+					case 'Ö':
+					case 'Ø':
+						result.Add('O');
+						result.Add('E');
+						continue;
 					// Ü -> UE
-					case Ü:
-						if (!inUmlaut)
-						{
-							c = U;
-							i--;
-						}
-						else
-						{
-							c = E;
-						}
-						inUmlaut = !inUmlaut;
-						break;
+					case 'Ü':
+						result.Add('U');
+						result.Add('E');
+						continue;
 					// Å -> AA
-					case Å:
-						if (!inUmlaut)
-						{
-							i--;
-						}
-						c = A;
-						inUmlaut = !inUmlaut;
-						break;
+					case 'Å':
+						result.Add('A');
+						result.Add('A');
+						continue;
 					// ß -> SS
-					case ß:
-						if (!inUmlaut)
-						{
-							i--;
-						}
-						c = S;
-						inUmlaut = !inUmlaut;
-						break;
+					case 'ß':
+						result.Add('S');
+						result.Add('S');
+						continue;
 				}
 				// We will remove all other characters
-				if (c < A || c > Z)
+				if ((int)input[i] < A || (int)input[i] > Z)
 				{
 					continue;
 				}
-				result.Append((char)c);
+				result.Add(input[i]);
 			}
-			return result.ToString();
+			return result.ToArray();
+		}
+
+		/// <summary>
+		/// Searches in a Square
+		/// </summary>
+		/// <param name="c">The char which I'm looking for...</param>
+		/// <param name="square">The square in which I'm looking for the char c...</param>
+		/// <returns>Returns the position in a two elements big byte-array.</returns>
+		private static int[] SearchInLine(char c, char[] line)
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				for (int j = 0; j < 5; j++)
+				{
+					if (line[(i * 5) + j] == c)
+					{
+						return new int[] { i, j };
+					}
+				}
+			}
+			return null;
 		}
 	}
 }
